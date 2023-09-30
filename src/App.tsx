859e85f1a27a194641c71929a0cfafdf0ae261f9
Jameel Personal Egbaria <jameel.eg@gmail.com>
2023-10-01 00:12:00 +0300

    Add donut chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Chart } from "react-google-charts";
 import './App.css';
+
+export const data = [
+  ["اسم المرشح", "عدد الاصوات"],
+  ["محمود جبارين - تكتك", 4],
+  ["حسن اغبارية", 3],
+  ["محمد جلال اغبارية", 2],
+  ["محمد تيسير قاروط", 2],
+];
+
+export const options = {
+  // title: "استطلاع رأي للانتخابات المحلية",
+  pieHole: 0.4,
+  is3D: true,
+  // backgroundColor: '#f1f3f4',
+  titleTextStyle: {
+    fontSize: 47,
+    bold: true,
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit bla bla <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="chart-header">
+        استطلاع رأي (סקר) لانتخابات المجلس المحلي طلعة عارة
+      </div>
+          <Chart
+              chartType="PieChart"
+              width="100%"
+              height="500px"
+              data={data}
+              options={options}
+          />
+
     </div>
   );
 }
